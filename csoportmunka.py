78fbc55d89d32e81ac9cd40806454045de9e094e
ANagyHoHoHoHorgasz <grcsmate@gmail.com>
2025-02-26 13:25:08 +0100

    Utazasi kalkulátor

---
+++ csoportmunka.py
@@ -0,0 +1,30 @@
+#Göröcs Máté, Ivády Barnabás, Kapitány Levente, Janovics Zombor
+import time
+#autopálya + főut, varos
+varosSebessegAtlag = 50 * 0.8
+foutSebessegAtlag = 110 * 0.8
+atlagsebessegAutoPalya = 130 * 0.9
+ErdTavVarosban = 8.8
+ErdTavFout = 0
+ErdTavAutoPalyan = 13.2
+ErdTavOssz = ErdTavVarosban + ErdTavFout + ErdTavAutoPalyan
+t1 = 13
+t2 = 0
+t3 = 7
+utazasiIdo = 2 * (t1 + t2 + t3 + 15)
+matricaAr = 5320
+benzinAr = 651
+fogyasztasVar = 0.095 * ErdTavVarosban
+fogyasztasFout = 0.07  * ErdTavFout
+fogyasztasAut = 0.085 * ErdTavAutoPalyan
+fogyasztasOssz = fogyasztasVar + fogyasztasFout + fogyasztasAut
+fogyasztasAr = 2 * (fogyasztasOssz * benzinAr)
+
+print("------------------------------------------")
+print("| Érdre fogunk menni                      |")
+print(f"|     -Az út hossza: {ErdTavOssz:.1f}km               |")
+print(f"|     -Az út időtartama: {utazasiIdo:.1f}perc         |")
+print(f"|     -Az autópálya matrica ára: {matricaAr:.1f}Ft |")
+print(f"|     -Az üzemanyagköltség: {fogyasztasAr:.1f}Ft      |")
+print(f"|     -Az út költsége:  {matricaAr + fogyasztasAr:.1f}Ft          |")
+print("------------------------------------------")
